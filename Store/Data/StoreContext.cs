e7e35f37e4d15452f4a4f04c341e9589fa25f0b1
Eric Couch <ericcouch@yahoo.com>
2022-12-05 11:54:20 -0600

    Add project files.

---
+++ Store/Data/StoreContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Store.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Data
+{
+    public class StoreContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; } = null!;
+        public DbSet<Order> Orders { get; set; } = null!;
+        public DbSet<Product> Products { get; set; } = null!;
+        public DbSet<OrderDetail> OrderDetails { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Database Connection String goes Here");
+        }
+    }
+}
